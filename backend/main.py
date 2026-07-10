635dafe17f1b619727b78a665143c2cea1db44ec
Jimbo Cai <cjimbo29@gmail.com>
2026-07-10 14:15:16 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,12 +18,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://solvestat.vercel.app",
-        "https://*.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
